1aed275722696474a39572ba30eb2a3905be8b54
Linus Henze <linus.henze@gmx.de>
2016-03-13 18:03:09 +0100

    Added OS X 10.11.4 support

--- Unrootless/Unrootless.c
+++ Unrootless/Unrootless.c
@@ -15,6 +15,7 @@
 #include "syscall.h"
 #include "kernel_info.h"
 #include "cpu_protections.h"
+#include "csr.h"
 
 kern_return_t Test_start(kmod_info_t * ki, void *d);
 kern_return_t Test_stop(kmod_info_t *ki, void *d);
@@ -22,10 +23,17 @@ kern_return_t Test_stop(kmod_info_t *ki, void *d);
 struct kernel_info g_kernel_info;
 extern const int version_major;
 
-static u_int32_t k_uint32 = 0;
-static int sysctl_rootless_uint32 SYSCTL_HANDLER_ARGS;
+static u_int32_t rootless_state = 0;
+static u_int32_t rootless_old_state = 0;
+static u_int32_t csr_flags = 0;
+static u_int32_t csr_orig_flags = 0;
+static u_int32_t csr_orig_state = 0;
+static int sysctl_rootless_disabled_func SYSCTL_HANDLER_ARGS;
+static int sysctl_rootless_csrFlags SYSCTL_HANDLER_ARGS;
 
-void (*_csr_set_allow_all)(int);
+void (*_csr_set_allow_all)(int) = NULL;
+
+
 
 SYSCTL_NODE(_debug, // our parent
             OID_AUTO , // automatically assign us an object ID
@@ -40,19 +48,94 @@ SYSCTL_PROC ( _debug_rootless, //our parent
              disabled, // our name
              ( CTLTYPE_INT | // type flag
               CTLFLAG_RW | CTLFLAG_ANYBODY ), //access flag ( read/write by anybody )
-             &k_uint32, // location of our data
+             &rootless_state, // location of our data
              0, //argument passed to our handler
-             sysctl_rootless_uint32, //our handler function
+             sysctl_rootless_disabled_func, //our handler function
              "IU", // our data type ( unsigned integer )
              "enable/disable rootless" // our description
 );
 
-static int sysctl_rootless_uint32 SYSCTL_HANDLER_ARGS {
-    
-    k_uint32 = (k_uint32 > 0) ? 1 : 0;
+SYSCTL_PROC ( _debug_rootless, //our parent
+             OID_AUTO , // automaticall assign us an object ID
+             csrConfig, // our name
+             ( CTLTYPE_INT | // type flag
+              CTLFLAG_RW | CTLFLAG_ANYBODY ), //access flag ( read/write by anybody )
+             &csr_flags, // location of our data
+             0, //argument passed to our handler
+             sysctl_rootless_csrFlags, //our handler function
+             "IU", // our data type ( unsigned integer )
+             "enable/disable rootless" // our description
+);
+
+kern_return_t setCSR(boolean_t flag) {
     enable_kernel_write();
-    _csr_set_allow_all(k_uint32);
+    if (rootless_state == 2) {
+        boot_args *args = (boot_args*) PE_state->bootArgs;
+        args->csrActiveConfig = csr_flags;
+        disable_kernel_write();
+        return KERN_SUCCESS;
+    }
+    if (_csr_set_allow_all != NULL) {
+        if (flag == TRUE) {
+            if (csr_orig_state == 1) { // Rootless was on on start
+                _csr_set_allow_all(0);
+            } else {
+                if (PE_state != NULL) {
+                    boot_args *args = (boot_args*) PE_state->bootArgs;
+                    args->csrActiveConfig = ROOTLESS_DEFAULT_ON;
+                }
+            }
+        } else {
+            _csr_set_allow_all(1);
+        }
+    } else if (PE_state != NULL) {
+        boot_args *args = (boot_args*) PE_state->bootArgs;
+        if (flag == TRUE) {
+            if (csr_orig_state == 1) { // Means rootless was on on start
+                args->csrActiveConfig = csr_orig_flags;
+            } else { // Means rootless was off on start
+                args->csrActiveConfig = ROOTLESS_DEFAULT_ON;
+            }
+        } else {
+            if (csr_orig_state == 1) { // Means rootless was on on start
+                args->csrActiveConfig = ROOTLESS_DEFAULT_OFF;
+            } else { // Means rootless was off on start
+                args->csrActiveConfig = csr_orig_flags;
+            }
+        }
+    }
     disable_kernel_write();
+    return KERN_SUCCESS;
+}
+
+static int sysctl_rootless_disabled_func SYSCTL_HANDLER_ARGS {
+    if (rootless_state == 2) { // Don't do anything
+        if (rootless_old_state != rootless_state && PE_state != NULL) {
+            rootless_old_state = 2;
+            sysctl_register_oid(&sysctl__debug_rootless_csrConfig);
+            csr_flags = rootless_old_state ? ROOTLESS_DEFAULT_OFF : ROOTLESS_DEFAULT_ON;
+        } else if (PE_state == NULL) {
+            rootless_state = rootless_old_state;
+        }
+        return sysctl_handle_int( oidp, oidp->oid_arg1 , oidp->oid_arg2 , req );
+    }
+    
+    if (rootless_old_state == 2) {
+        sysctl_unregister_oid(&sysctl__debug_rootless_csrConfig);
+    }
+    
+    rootless_state = (rootless_state > 0) ? 1 : 0;
+    
+    setCSR(!rootless_state);
+    
+    rootless_old_state = rootless_state;
+    
+    return sysctl_handle_int( oidp, oidp->oid_arg1 , oidp->oid_arg2 , req );
+}
+
+static int sysctl_rootless_csrFlags SYSCTL_HANDLER_ARGS {
+    csr_flags = csr_flags & CSR_VALID_FLAGS;
+    setCSR(1); // Value doesn't care...
     
     return sysctl_handle_int( oidp, oidp->oid_arg1 , oidp->oid_arg2 , req );
 }
@@ -74,15 +157,33 @@ kern_return_t unrootless_start(kmod_info_t * ki, void *d)
     {
         return KERN_FAILURE;
     }
+    
     _csr_set_allow_all=(void*)solve_kernel_symbol(&g_kernel_info, "_csr_set_allow_all");
     if (_csr_set_allow_all == NULL) {
         LOG_ERROR("Couldn't find '_csr_set_allow_all'");
-        return KERN_FAILURE;
+        LOG_INFO("Trying _PE_state instead...");
     }
+    
+    PE_state = (PE_state_t*) (solve_kernel_symbol(&g_kernel_info, "_PE_state"));
+    if (PE_state == NULL) {
+        if (_csr_set_allow_all == NULL) {
+            LOG_ERROR("Couldn't find '_PE_state' and '_csr_set_allow_all', aborting...");
+            return KERN_FAILURE;
+        }
+    } else {
+        csr_orig_flags = ((boot_args*) PE_state->bootArgs)->csrActiveConfig;
+        csr_flags = csr_orig_flags;
+    }
+    
     int(*_csr_check)(int);
     _csr_check = (void*)solve_kernel_symbol(&g_kernel_info, "_csr_check");
-    _csr_set_allow_all(1);
-    k_uint32 = !_csr_check(0);
+    if (_csr_set_allow_all != NULL) {
+        rootless_state = !_csr_check(0);
+        csr_orig_state = _csr_check(0);
+    } else {
+        rootless_state = (csr_orig_flags & CSR_VALID_FLAGS) == 0 ? 0 : 1;
+        csr_orig_state = (csr_orig_flags & CSR_VALID_FLAGS) == 0 ? 1 : 0;
+    }
     
     sysctl_register_oid(&sysctl__debug_rootless);
     sysctl_register_oid(&sysctl__debug_rootless_disabled);
@@ -92,9 +193,13 @@ kern_return_t unrootless_start(kmod_info_t * ki, void *d)
 
 kern_return_t unrootless_stop(kmod_info_t *ki, void *d)
 {
-    cleanup_sysent();
-    _csr_set_allow_all(0);
+    //cleanup_sysent();
+    //_csr_set_allow_all(0);
+    setCSR(csr_orig_state);
     sysctl_unregister_oid(&sysctl__debug_rootless);
     sysctl_unregister_oid(&sysctl__debug_rootless_disabled);
+    if (rootless_state == 2) {
+        sysctl_unregister_oid(&sysctl__debug_rootless_csrConfig);
+    }
     return KERN_SUCCESS;
 }
---
+++ Unrootless/csr.h
@@ -0,0 +1,118 @@
+//
+//  csr.h
+//  Unrootless
+//
+//  Created by Linus Henze on 13/03/16.
+//  Copyright © 2016 Linus Henze. All rights reserved.
+//
+
+#ifndef csr_h
+#define csr_h
+
+struct PE_Video {
+    unsigned long   v_baseAddr;     /* Base address of video memory */
+    unsigned long   v_rowBytes;     /* Number of bytes per pixel row */
+    unsigned long   v_width;        /* Width */
+    unsigned long   v_height;       /* Height */
+    unsigned long   v_depth;        /* Pixel Depth */
+    unsigned long   v_display;      /* Text or Graphics */
+    char		v_pixelFormat[64];
+    long		v_resv[ 4 ];
+};
+
+typedef struct PE_Video       PE_Video;
+
+typedef struct PE_state {
+    boolean_t	initialized;
+    PE_Video	video;
+    void		*deviceTreeHead;
+    void		*bootArgs;
+#if __i386__
+    void		*fakePPCBootArgs;
+#endif
+} PE_state_t;
+
+PE_state_t *PE_state = NULL;
+
+#define BOOT_LINE_LENGTH        1024
+
+struct Boot_Video {
+    uint32_t	v_baseAddr;	/* Base address of video memory */
+    uint32_t	v_display;	/* Display Code (if Applicable */
+    uint32_t	v_rowBytes;	/* Number of bytes per pixel row */
+    uint32_t	v_width;	/* Width */
+    uint32_t	v_height;	/* Height */
+    uint32_t	v_depth;	/* Pixel Depth */
+};
+
+typedef struct Boot_Video	Boot_Video;
+
+typedef struct boot_args {
+    uint16_t    Revision;	/* Revision of boot_args structure */
+    uint16_t    Version;	/* Version of boot_args structure */
+    
+    uint8_t     efiMode;    /* 32 = 32-bit, 64 = 64-bit */
+    uint8_t     debugMode;  /* Bit field with behavior changes */
+    uint16_t    flags;
+    
+    char        CommandLine[BOOT_LINE_LENGTH];	/* Passed in command line */
+    
+    uint32_t    MemoryMap;  /* Physical address of memory map */
+    uint32_t    MemoryMapSize;
+    uint32_t    MemoryMapDescriptorSize;
+    uint32_t    MemoryMapDescriptorVersion;
+    
+    Boot_Video	Video;		/* Video Information */
+    
+    uint32_t    deviceTreeP;	  /* Physical address of flattened device tree */
+    uint32_t    deviceTreeLength; /* Length of flattened tree */
+    
+    uint32_t    kaddr;            /* Physical address of beginning of kernel text */
+    uint32_t    ksize;            /* Size of combined kernel text+data+efi */
+    
+    uint32_t    efiRuntimeServicesPageStart; /* physical address of defragmented runtime pages */
+    uint32_t    efiRuntimeServicesPageCount;
+    uint64_t    efiRuntimeServicesVirtualPageStart; /* virtual address of defragmented runtime pages */
+    
+    uint32_t    efiSystemTable;   /* physical address of system table in runtime area */
+    uint32_t    kslide;
+    
+    uint32_t    performanceDataStart; /* physical address of log */
+    uint32_t    performanceDataSize;
+    
+    uint32_t    keyStoreDataStart; /* physical address of key store data */
+    uint32_t    keyStoreDataSize;
+    uint64_t	bootMemStart;
+    uint64_t	bootMemSize;
+    uint64_t    PhysicalMemorySize;
+    uint64_t    FSBFrequency;
+    uint64_t    pciConfigSpaceBaseAddress;
+    uint32_t    pciConfigSpaceStartBusNumber;
+    uint32_t    pciConfigSpaceEndBusNumber;
+    uint32_t	csrActiveConfig;
+    uint32_t	csrPendingConfig;
+    uint32_t    __reserved4[728];
+    
+} boot_args;
+
+#define CSR_ALLOW_UNTRUSTED_KEXTS		(1 << 0)
+#define CSR_ALLOW_UNRESTRICTED_FS		(1 << 1)
+#define CSR_ALLOW_TASK_FOR_PID			(1 << 2)
+#define CSR_ALLOW_KERNEL_DEBUGGER		(1 << 3)
+#define CSR_ALLOW_APPLE_INTERNAL		(1 << 4)
+#define CSR_ALLOW_DESTRUCTIVE_DTRACE	(1 << 5) /* name deprecated */
+#define CSR_ALLOW_UNRESTRICTED_DTRACE	(1 << 5)
+#define CSR_ALLOW_UNRESTRICTED_NVRAM	(1 << 6)
+
+#define CSR_VALID_FLAGS (CSR_ALLOW_UNTRUSTED_KEXTS | \
+CSR_ALLOW_UNRESTRICTED_FS | \
+CSR_ALLOW_TASK_FOR_PID | \
+CSR_ALLOW_KERNEL_DEBUGGER | \
+CSR_ALLOW_APPLE_INTERNAL | \
+CSR_ALLOW_UNRESTRICTED_DTRACE | \
+CSR_ALLOW_UNRESTRICTED_NVRAM)
+
+#define ROOTLESS_DEFAULT_ON  0
+#define ROOTLESS_DEFAULT_OFF 103
+
+#endif /* csr_h */
